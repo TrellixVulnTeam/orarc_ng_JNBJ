ccb5b49ebc7ff2f5ae4adfaf74a935993c220ae2
egawa <egawa@ubiquitous-tech.com>
2022-03-11 19:04:04 +0900

    1st test

---
+++ src/app/app.component-ng.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root-ng',
+  templateUrl: './app.component-ng.html',
+  styleUrls: ['./app.component-ng.css']
+})
+export class AppComponent {
+  title = 'orarc_ng';
+}
